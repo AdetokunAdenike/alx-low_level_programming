0ac7edc6a8b306ca4ef983d002f6cf1b6e9e35c1
Adenike <adetokunadenike@gmail.com>
2023-09-15 23:19:01 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
 int main() {
     puts("Programming is like building a multilingual puzzle");
     return 0;
