f4d6edaf9c0b2a5dba3804e2f6556ffbe87250ed
Adenike <adetokunadenike@gmail.com>
2023-09-21 04:12:05 -0700

    function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,52 +10,58 @@
  * Return: String with all letters in ROT13 base.
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
-	
-	while (n1[a_len] != '\0')
-		a_len++;
-	while (n2[b_len] != '\0')
-		b_len++;
-	if (a_len > b_len)
-		biggest = a_len;
-	else
-		biggest = b_len;
-	if ((biggest + 1) >= size_r)
-		return (0);
-	r[biggest + 1] = '\0';
-	
-	while (biggest >= 0)
-	{
-		a = (n1[a_len - 1] - '0');
-		b = (n1[b_len - 1] - '0');
-		if (a_len > 0 && b_len > 0)
-			sum = a + b + carry;
-		else if (a_len < 0 && b_len > 0)
-			sum = b + carry;
-		else if (a_len > 0 && b_len < 0)
-			sum = a + carry;
-		else
-			sum = carry;
-		
-		if (sum > 9)
-		{
-			carry = sum / 10;
-			sum = (sum % 10) + '0';
-		}
-		else
-		{
-			carry = 0;
-			sum = sum + '0';
-		}
-		r[biggest] = sum;
-		a_len--;
-		b_len--;
-		biggest--;
-	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+char* infinite_add(char* n1, char* n2, char* r, int size_r) {
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    
+    // Check if the result can fit in the buffer
+    if (len1 + len2 >= size_r)
+        return 0;
+
+    int carry = 0;  // Initialize carry to 0
+    int i = len1 - 1;
+    int j = len2 - 1;
+    int k = 0;  // Index for the result
+
+    // Add digits from right to left
+    while (i >= 0 || j >= 0 || carry) {
+        int num1 = (i >= 0) ? (n1[i] - '0') : 0;
+        int num2 = (j >= 0) ? (n2[j] - '0') : 0;
+        int sum = num1 + num2 + carry;
+
+        carry = sum / 10;
+        r[k] = (sum % 10) + '0';
+
+        i--;
+        j--;
+        k++;
+    }
+
+    r[k] = '\0';
+
+    // Reverse the result string
+    for (int start = 0, end = k - 1; start < end; start++, end--) {
+        char temp = r[start];
+        r[start] = r[end];
+        r[end] = temp;
+    }
+
+    return r;
 }
+
+int main() {
+    char n1[] = "12345";
+    char n2[] = "67890";
+    int size_r = 20;
+    char r[size_r];
+
+    char* result = infinite_add(n1, n2, r, size_r);
+    if (result != 0) {
+        printf("Sum: %s\n", result);
+    } else {
+        printf("Result cannot be stored in the buffer.\n");
+    }
+
+    return 0;
+}
+
