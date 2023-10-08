0d89993d0e73d3875cd5eebf974c9b2ac7d8480f
Adenike <adetokunadenike@gmail.com>
2023-10-07 20:49:24 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,6 +4,8 @@
 
 /**
  * main - a program that adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
 int main(int argc, char *argv[])
