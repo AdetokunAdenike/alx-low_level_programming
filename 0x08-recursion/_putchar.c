bd359cdd7205b18863590c77531b0b3530f0352a
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-16 13:26:13 +0100

    recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+   * _putchar - writes the character c to stdout
+    * @c: output
+     *
+      * Return 1 on success.
+       * On error, -1 is returned, and error is set appropriately.
+        */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
