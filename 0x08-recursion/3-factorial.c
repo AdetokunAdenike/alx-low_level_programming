b679084442ccc6527943e50a52cc0b7e6f6d8721
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-16 14:48:15 +0100

    fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,22 +2,16 @@
 
 /**
   * factorial - function parameter
-  * @n: parameter
-  * Returns: function n.
+  *
+  * @n: function parameter
+  *
+  * Return: Function n.
   */
-
 int factorial(int n)
 {
 	if (n < 0)
-	{
 	return (-1);
-	}
 	if (n == 0)
-	{
 	return (1);
-	}
-	else 
-	{
 	return (n * factorial(n - 1));
-	}
 }
