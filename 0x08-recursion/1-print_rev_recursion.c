7a3a81434ce45866cc0aa41f22ffd2da2a9167fa
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-16 08:23:23 +0100

    recursion updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _print_rev_recursion - Prints a string in reverse
  * @s: input parameter
@@ -11,5 +10,9 @@ void _print_rev_recursion(char *s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
+	}
+	else
+	{
+		_putchar(\n);
 	}
 }
