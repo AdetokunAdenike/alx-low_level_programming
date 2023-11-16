045865eea7ec855403593ea358e12c89d8aa8d97
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-16 14:10:47 +0100

    fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
 /**
-  * _print_rev_recursion - main function
-  * @s: Function parameter
-  * Return: Void
-  */
+ * _print_rev_recursion - main function
+ * @s: parameter
+ * Return: Void
+*/
+
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+    if (*s)
 	{
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+        return;
+
+    _print_rev_recursion(s + 1);
+    _putchar(*s);
+   }
+   else
+	{
+		_putchar('\n');
 	}
 }
