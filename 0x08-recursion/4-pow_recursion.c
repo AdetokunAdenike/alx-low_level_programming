e1bc278feb0a0e8eb7ba9ad8d05eaa7d8cd34ee1
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-16 15:13:10 +0100

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: value
  * @y: power
- * Returns the result of the power
+ * Returns: the result of the power
  */
 int _pow_recursion(int x, int y)
 {
