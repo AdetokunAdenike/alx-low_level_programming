0b018a4efddd7198ee64ef3c68a79f8b29dee75f
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-20 01:12:29 +0100

    update create_static_lib.sh

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ * Returns non-zero if true
+ * Returns zero if false
+ */
+int _isupper(int c)
+{
+    return isupper(c);
+}
