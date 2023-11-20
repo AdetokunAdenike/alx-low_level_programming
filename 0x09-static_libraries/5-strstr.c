9ace7ed4a59e0a36ce4b1f29cfaeac0113d5569a
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-20 04:40:31 +0100

    update

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -5,31 +5,28 @@
  * @haystack: pointer to the string to be searched
  * @needle: pointer to the substring to search for
  * Return: pointer to the beginning of the located substring,
- *         or NULL if the substring is not found
+ * or a pointer to the end of the haystack if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-    char *haystack_ptr;
-    char *needle_ptr;
+    char *h, *n;
 
-    while (*haystack != '\0')
+    while (*haystack)
     {
-        haystack_ptr = haystack;
-        needle_ptr = needle;
+        h = haystack;
+        n = needle;
 
-        while (*needle_ptr != '\0' && *haystack_ptr == *needle_ptr)
+        while (*haystack && *n && *haystack == *n)
         {
-            haystack_ptr++;
-            needle_ptr++;
+            haystack++;
+            n++;
         }
 
-        if (*needle_ptr == '\0')
-        {
-            return haystack;  // Return a pointer to the beginning of the located substring
-        }
+        if (!*n)
+            return h;
 
-        haystack++;
+        haystack = h + 1;
     }
 
-    return NULL;  // Return NULL if the substring is not found
+    return haystack; // Pointer to the end of haystack if substring is not found
 }
