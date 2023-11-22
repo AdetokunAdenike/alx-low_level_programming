d6b69225e87a06de0604f3834c5c9395ac5b67be
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-22 11:56:26 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,7 @@
  * @s1: first parameter.
  * @s2: second parameter.
  *
- * Returns: Returns character pointer upon success, NULL otherwise.
+ * Returns: returns character pointer upon success, NULL otherwise.
 */
 
 char *str_concat(char *s1, char *s2)
