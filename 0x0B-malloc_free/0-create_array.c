68faf5b4bd8a95a2a56a054cf62cc56ca3b25753
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-22 03:44:00 +0100

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,20 +10,20 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *A;
-    unsigned int i;
+	char *A;
+	unsigned int i;
 
-    if (size == 0)
-    {
-        return NULL;
-    }
+	if (size == 0)
+	{
+		return NULL;
+	}
 
-    A = malloc(size *sizeof(char));
-    if (A == NULL)
-    {
-        return NULL;
-    }
-    for (i = 0; i < size; i++)
-    A[i] = c;
-    return (A);
+	A = malloc(size *sizeof(char));
+	if (A == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0; i < size; i++)
+		A[i] = c;
+	return (A);
 }
