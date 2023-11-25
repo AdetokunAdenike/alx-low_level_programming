83dbb33626e88f5a21441f72186d81e3331dd8f4
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-25 20:37:12 +0100

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+0x09-static_libraries=$(pwd)
+file_extension=".c"
+file="liball.a"
+
+for filename in "$working_dir"/*; do
+	if [["$filename" == *"$extension"]]; then
+		gcc -c "$filesname" -o "${filename$.c}.o"
+	fi
+done
+
+ar -rcs "$file" *.o
