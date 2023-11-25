11f13732c0e5401dd0631e263c1cbb232f4703ae
AdetokunAdenike <adetokunadenike@gmail.com>
2023-11-25 20:51:20 +0100

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -4,7 +4,7 @@ file_extension=".c"
 file="liball.a"
 
 for filename in "$working_dir"/*; do
-	if [["$filename" == *"$extension"]]; then
+	if [["$filename" == *"$file_extension"]]; then
 		gcc -c "$filename" -o "${filename%.c}.o"
 	fi
 done
