e97e1820de0569517a72c6125ee879bdec9edc18
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-21 18:48:42 +0100

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t pos, low, high;
-	double f;
+	double fraction;
 
 	if (array == NULL)
 		return (-1);
@@ -24,8 +24,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (size)
 	{
-		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-		pos = (size_t)(low + f);
+		fraction = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + fraction);
 		printf("Value checked array[%d]", (int)pos);
 
 		if (pos >= size)
