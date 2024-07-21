bcb09c65920c4606557ad026199f52efde035a2a
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-21 03:29:11 +0100

    Update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,6 +9,7 @@
  * 
  * Return: First index where value is located, or -1 if value not found or array is NULL
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
@@ -17,10 +18,10 @@ int linear_search(int *array, size_t size, int value)
 	return (-1);
 
 	for (i = 0; i < size; i++)
-    {
+	{
 	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 	if (array[i] == value)
 		return (i);
-    }
+	}
 	return (-1);
 }
